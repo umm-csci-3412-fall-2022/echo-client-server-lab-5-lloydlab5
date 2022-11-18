ec6868e5014be43dcf66deb6657cf730c4509061
Lloyd Hilsgen <hilsg008@mylar.morris.umn.edu>
2022-11-17 18:53:29 -0600

    Unsure how to close server after client disconnects

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,51 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) throws IOException {
+    String server;
+    // Use "127.0.0.1", i.e., localhost, if no server is specified.
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+    try {
+      // Connect to the server
+      Socket socket = new Socket(server, portNumber);
+
+      // Get the input stream so we can read from that socket
+      InputStream input = socket.getInputStream();
+      PrintWriter serverWriter = new PrintWriter(socket.getOutputStream(), true);
+      BufferedReader serverReader = new BufferedReader(new InputStreamReader(input));
+      BufferedReader keyboardReader = new BufferedReader(new InputStreamReader(System.in));
+
+      // Send information to the server
+      String line;      
+      while((line = keyboardReader.readLine()) != null) {
+        serverWriter.println(line);
+        String recieved = serverReader.readLine();
+        System.out.println(recieved);
+      } 
+
+      // Close the socket when we're done reading from it
+      socket.shutdownInput();
+      socket.shutdownOutput();
+      serverReader.close();
+      serverWriter.close();
+      keyboardReader.close();
+
+    // Provide some minimal error handling.
+    } catch (ConnectException ce) {
+      System.out.println("We were unable to connect to " + server);
+      System.out.println("You should make sure the server is running.");
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,43 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+  public static final int portNumber = 6013;
+
+  public static void main(String[] args) {
+    try {
+      // Start listening on the specified port
+      ServerSocket sock = new ServerSocket(portNumber);
+
+      // Run forever, which is common for server style services
+      while (true) {
+        // Wait until someone connects, thereby requesting a date
+        Socket client = sock.accept();
+        System.out.println("Connected:");
+
+        // Construct a reader to grab info sent to the server
+        BufferedReader clientReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        // Construct a writer so we can write to the socket, thereby
+        // sending something back to the client.
+        PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+        // Grab information from the client and send it back
+        String line;      
+        while((line = clientReader.readLine()) != null) {
+          writer.println(line);
+        }
+
+        // Close the client socket and reader since we're done.
+        writer.close();
+        clientReader.close();
+        client.close();
+      }
+    // *Very* minimal error handling.
+    } catch (IOException ioe) {
+      System.out.println("We caught an unexpected exception");
+      System.err.println(ioe);
+    }
+  }
 }
